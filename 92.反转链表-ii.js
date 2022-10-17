622727607fce2a81cce7cd5bb7c1ad6b0704f8f3
roy guo <royguo@tencent.com>
2022-10-17 22:04:22 +0800

    chained list

---
+++ 92.反转链表-ii.js
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode.cn id=92 lang=javascript
+ *
+ * [92] 反转链表 II
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} left
+ * @param {number} right
+ * @return {ListNode}
+ */
+var reverseBetween = function(head, left, right) {
+  let dummy = new ListNode(-1);
+  dummy.next= head;
+  let pre = dummy;
+
+  for (let i=0; i<left-1; ++i) {
+    pre = pre.next //find node before left
+  }
+
+  let current = pre.next
+
+  for(let i=0; i<right-left;i++) { //可以估算出要循环的次数。
+    let t = current.next;
+    current.next = t.next
+    t.next = pre.next 
+    pre.next = t;
+  }
+  return dummy.next;
+
+};
+// @lc code=end
+
